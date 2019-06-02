0e253c25af9d3e92a6070fdc9ba984af289fae6e
Michel Gabriel <michel.gabriel15@gmail.com>
2019-06-02 15:46:29 +0200

    Setup folder structure

---
+++ src/components/app/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Order from "./order";
+import "./app.scss";
+
+const App = () => {
+  return (
+    <div className="app">
+      <Order />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/app/order/order-container.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class OrderContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+  render() {
+    return (
+      <div className="container">
+        <h1>Restaurant Menu</h1>
+      </div>
+    );
+  }
+}
