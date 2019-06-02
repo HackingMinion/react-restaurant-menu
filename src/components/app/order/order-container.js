4ee37c6c57aec2f9f710c08df02de7b6a785d47a
Michel Gabriel <michel.gabriel15@gmail.com>
2019-06-02 23:36:30 +0200

    Add basic iteration over course items

--- src/components/app/order/order-container.js
+++ src/components/app/order/order-container.js
@@ -1,17 +1,51 @@
-import React, { Component } from 'react';
+import React, { Component } from "react"
 
 export default class OrderContainer extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
-      
-    };
+      currentCourse: 0,
+      data: []
+    }
   }
+
+  componentDidMount() {
+    this.setState({
+      data: require("../../../assets/data/fe-tech-data.json")
+    }, () => this.handleGetCourseItems())
+  }
+
+  getItems = () => {
+    const data = this.state.data
+    let items = data.filter(res => {
+      return res.courseType.includes(2) 
+    })
+    console.log(items)
+    this.setState({
+      data: items
+    })
+  }
+
+  handleGetCourseItems = (courseNumber = 0) => {
+    this.setState({
+        currentCourse: courseNumber
+      }, () => this.getItems()
+    )
+  }
+
   render() {
+    const data = this.state.data
     return (
       <div className="container">
-        <h1>Restaurant Menu</h1>
+        <h1 className="title">Restaurant Menu</h1>
+        <div>
+          {data.map(res => (
+            <p>
+              {res.id} {res.title}
+            </p>
+          ))}
+        </div>
       </div>
-    );
+    )
   }
 }
