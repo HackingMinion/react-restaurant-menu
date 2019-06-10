af2a6ee589da5c34011bc160ead475e88b6e85e0
Michel Gabriel <michel.gabriel15@gmail.com>
2019-06-10 15:31:07 +0200

    Use app class as course handler

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,13 +1,30 @@
-import React from "react";
-import Order from "./order";
-import "./app.scss";
+import React, { Component } from "react"
+import Order from "./order"
 
-const App = () => {
-  return (
-    <div className="app">
-      <Order />
-    </div>
-  );
-};
+import "./app.scss"
 
-export default App;
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      course: 4
+    }
+  }
+
+  handleCourse = (course = 0) => {
+    this.setState({ course: course })
+    console.log("app: " + course)
+  }
+
+  render() {
+    const course = this.state.course
+    return (
+      <main className="app">
+        <Order
+          course={course}
+          changeCourse={this.handleCourse}
+        />
+      </main>
+    )
+  }
+}
