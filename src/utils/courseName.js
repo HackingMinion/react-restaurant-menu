eee0148df1e613325a9b1f896cc8a924247a7c1b
Michel Gabriel <michel.gabriel15@gmail.com>
2019-06-10 18:57:58 +0200

    Refactor util

--- src/utils/courseName.js
+++ src/utils/courseName.js
@@ -1,11 +1,18 @@
-export default function getCourseName(number) {
-  const courses = {
-    0: "Hors d'oeuvres",
-    1: "Soup",
-    2: "Fish",
-    3: "Salad",
-    4: "Main Course",
-    5: "Dessert"
-  }
+const courses = [
+  "Hors d'oeuvres",
+  "Soup",
+  "Fish",
+  "Salad",
+  "Main Course",
+  "Dessert"
+]
+
+function getCourseName(number) {
   return courses[number]
 }
+
+function getAllCourseNames() {
+  return courses
+}
+
+export { getAllCourseNames, getCourseName }
