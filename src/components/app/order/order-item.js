984dc43d35cede90957c8eef73ce442cfdb2154d
Michel Gabriel <michel.gabriel15@gmail.com>
2019-06-15 19:04:47 +0200

    Formatting

--- src/components/app/order/order-item.js
+++ src/components/app/order/order-item.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react"
 import Card from "@material-ui/core/Card"
 import CardActionArea from "@material-ui/core/CardActionArea"
-import CardActions from "@material-ui/core/CardActions"
 import CardContent from "@material-ui/core/CardContent"
 import CardMedia from "@material-ui/core/CardMedia"
-import Button from "@material-ui/core/Button"
 import Typography from "@material-ui/core/Typography"
 
 export default class OrderItem extends Component {
   handleClick = () => {
+    /* this.classList.contains('-selected'); */
     this.props.itemHandler(this.props.id, this.props.itemTitle)
   }
 
   render() {
     return (
-      <Card className={`${this.props.className} ${this.props.selected ? "-selected" : ""}`}>
+      <Card
+        className={`${this.props.className} ${
+          this.props.selected ? "-selected" : ""
+        }`}
+      >
         <CardActionArea onClick={this.handleClick}>
           <CardMedia
             component="img"
@@ -24,10 +27,20 @@ export default class OrderItem extends Component {
             title={this.props.itemTitle}
           />
           <CardContent>
-            <Typography gutterBottom variant="h6" component="h2" className="item-title">
+            <Typography
+              gutterBottom
+              variant="h6"
+              component="h2"
+              className="item-title"
+            >
               {this.props.itemTitle}
             </Typography>
-            <Typography variant="body2" color="textSecondary" component="p" className="item-desc">
+            <Typography
+              variant="body2"
+              color="textSecondary"
+              component="p"
+              className="item-desc"
+            >
               {this.props.itemDescription}
             </Typography>
           </CardContent>
