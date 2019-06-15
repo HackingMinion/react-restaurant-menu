2efd73ccf44f12b855615822962fa2e2065b37fc
Michel Gabriel <michel.gabriel15@gmail.com>
2019-06-15 19:06:32 +0200

    Rename variables

--- src/utils/courseItem.js
+++ src/utils/courseItem.js
@@ -1,5 +1,5 @@
-export default function getCourseItems(data, courseType = 0) {
-  let items = data.filter(res => {
+export default function getCourseItems(courseItems, courseType = 0) {
+  let items = courseItems.filter(res => {
     return res.courseType.includes(courseType)
   })
   return items
