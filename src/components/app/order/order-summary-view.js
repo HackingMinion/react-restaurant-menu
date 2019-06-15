fc8e3a7d424659bada1de320d3afcd07dee0cbe3
Michel Gabriel <michel.gabriel15@gmail.com>
2019-06-15 19:07:39 +0200

    Add summary view component

---
+++ src/components/app/order/order-summary-view.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+
+export default class OrderSummaryView extends Component {
+  render() {
+    return (
+      <div className="order-summary-view">
+        <h4 className="title">{this.props.courseTitle}</h4>
+        <ul>
+          {this.props.courseItems.map(item => (
+            <li>{item.title}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
--- src/components/app/order/order-summary.js
+++ src/components/app/order/order-summary.js
@@ -1,8 +1,23 @@
 import React, { Component } from "react"
+import { getCourseName } from "../../../utils/courseName"
+import OrderSummaryView from "./order-summary-view"
 
 export default class OrderSummary extends Component {
   render() {
     const { selectedItems } = this.props
-    return Object.keys(selectedItems).map(item => <p>{selectedItems[item]}</p>)
+    return Object.keys(selectedItems).map(item => {
+      if (selectedItems[item].length !== 0) {
+        let items = []
+        for (let i = 0; i < selectedItems[item].length; i++) {
+          items.push(selectedItems[item][i])
+        }
+        return (
+          <OrderSummaryView
+            courseTitle={getCourseName(item)}
+            courseItems={items}
+          />
+        )
+      }
+    })
   }
 }
